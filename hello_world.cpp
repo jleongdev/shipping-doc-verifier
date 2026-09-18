f3875e01aab41561ba274e91a9100b9098729fb5
Jason <jason.extraclass@gmail.com>
2026-09-18 23:46:48 +0800

    A message to Jia Wei

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Jia Wei and Ms Aparna shares a Unique bond :)" << endl;
+
+    return 0;
+}
