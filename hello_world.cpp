6a9dee5ae510dcc5ac015df2e7cee96e855dbfc0
Poon Shi Xun <ember7243@gmail.com>
2026-09-19 15:09:07 +0800

    Added another fun message for Lim Jia Wei

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,5 +6,7 @@ int main()
     cout << "Hello World!" << endl;
     cout << "Jia Wei and Ms Aparna shares a Unique bond :)" << endl;
 
+    cout << "Ms Aparna is Jia Wei's favourite teacher" << endl;
+
     return 0;
 }
